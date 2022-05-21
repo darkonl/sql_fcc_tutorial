a53a672fbc8e0b97a7366c3e23b248899eadd171
Nahuel PeFu <daro.nahuel@gmail.com>
2022-05-21 15:53:36 +0200

    feat: update joins

--- part5-6_join.sql
+++ part5-6_join.sql
@@ -5,3 +5,25 @@
 
 INSERT INTO branch VALUES(4, 'Buffalo', NULL, NULL);
 SELECT * FROM branch;
+
+-- FIND ALL BRANCHES AND THE NAMES OF THEIR MANAGERS
+SELECT employee.emp_id, employee.first_name, branch.branch_name
+FROM employee
+JOIN branch
+ON employee.emp_id = branch.mgr_id; --> JOIN
+
+-- FIND ALL BRANCHES AND THE NAMES OF THEIR MANAGERS
+SELECT employee.emp_id, employee.first_name, branch.branch_name
+FROM employee
+LEFT JOIN branch
+ON employee.emp_id = branch.mgr_id
+ORDER BY branch.branch_name DESC; --> LEFT JOIN
+
+
+-- FIND ALL BRANCHES AND THE NAMES OF THEIR MANAGERS
+SELECT employee.emp_id, employee.first_name, branch.branch_name
+FROM employee
+RIGHT JOIN branch
+ON employee.emp_id = branch.mgr_id
+ORDER BY branch.branch_name DESC; --> RIGHT JOIN
+
